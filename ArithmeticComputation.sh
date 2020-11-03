0ebbe6e614cf744ac6d9a933e351b3269a510343
vamshi7589 <vamshi7589@gmail.com>
2020-11-03 14:04:08 +0530

    added: descending order added

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -20,3 +20,22 @@ function ascendingOrderSort()
    done
    echo "To data ascending order:"${array[@]}
 }
+#TO FUNCTION SORTING RESULTS IN DECENDING ORDER
+function descendingOrderSort()
+{
+	for(( index=0; index<${#array[@]}; index ++ ))
+	do
+		for(( indexOne=0; indexOne<${#array[@]}-1; indexOne ++ ))
+		do
+			if (( $(echo "${array[indexOne+1]} > ${array[indexOne]}" | bc -l ) ))
+			then
+				temp=${array[indexOne]}
+				array[indexOne]=${array[indexOne+1]}
+				array[indexOne+1]=$temp
+			fi
+		done
+	done
+echo "To data descending order:${array[@]}"
+}
+#TO FUNCTION CALL FOR SORTING IN DECENDING ORDER
+descendingOrderSort ${array[@]}
