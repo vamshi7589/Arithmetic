fe5649414347842358b525cfc8431b343f0f3d33
vamshi7589 <vamshi7589@gmail.com>
2020-11-03 14:00:11 +0530

    added: ascending order added

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -3,4 +3,20 @@ for((index=0; index<=${#arithmaticOperation[@]}; index++)) do
 done
 #TO PRINT THE ARRAY
 echo "${array[@]}"
-
+#TO FUNCTION SORTING RESULTS IN ASCENDING ORDER
+function ascendingOrderSort()
+{
+   for(( index=0; index<${#array[@]}; index ++ ))
+   do
+      for(( indexOne=0; indexOne<${#array[@]}-1; indexOne ++ ))
+      do
+         if (( $(echo "${array[indexOne+1]} < ${array[indexOne]}" | bc -l ) ))
+         then
+            temp=${array[indexOne]}
+            array[indexOne]=${array[indexOne+1]}
+            array[indexOne+1]=$temp
+         fi
+      done
+   done
+   echo "To data ascending order:"${array[@]}
+}
