e68266fa3e1eb35cb4d1c810647d91b4c48fa991
vamshi7589 <vamshi7589@gmail.com>
2020-11-03 13:56:42 +0530

    added: reading into array

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,22 +1,6 @@
-echo "*************welcome to arithmetic computation and sorting **********"
-#TO DECLAER THE DICTIONARY
-declare -A arithmaticOperation
+for((index=0; index<=${#arithmaticOperation[@]}; index++)) do 
+  array[index]=${arithmaticOperation[result$((index+1))]}
+done
+#TO PRINT THE ARRAY
+echo "${array[@]}"
 
-#TO TAKING INPUT FROM USER
-read -p "Enter first input :" a
-read -p "Enter second input:" b
-read -p "Enter third input :" c
-#COMPUTE ARITHMETIC OPERATION
-result1=$(( a + b * c ))
-echo "Result:" $result1
-result2=$(( a * b + c ))
-echo "Result:" $result2
-result3=$(( c + a / b ))
-echo "Result:" $result3
-result4=$(( a % b + c ))
-echo "Result:" $result4
-#TO STORE THE DICTIONARY
-arithmaticOperation[result1]=$result1
-arithmaticOperation[result2]=$result2
-arithmaticOperation[result3]=$result3
-arithmaticOperation[result4]=$result4
