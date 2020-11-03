04135ffd7837afc868cabe838366fca3636bb168
vamshi7589 <vamshi7589@gmail.com>
2020-11-03 13:15:23 +0530

    added: inputs to arithmetic computation

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -1,1 +1,8 @@
 echo "*************welcome to arithmetic computation and sorting **********"
+#TO DECLAER THE DICTIONARY
+declare -A arithmaticOperation
+
+#TO TAKING INPUT FROM USER
+read -p "Enter first input :" a
+read -p "Enter second input:" b
+read -p "Enter third input :" c
